f1ec7ad4eaa7572fb3a1260933dfe891da3c6b9e
Rachel Cynthia V <rachel883omega@gmail.com>
2025-05-30 19:26:06 +0100

    change icon, minor fixes

--- src/components/Portfolio/AboutText/AboutText.js
+++ src/components/Portfolio/AboutText/AboutText.js
@@ -1,11 +1,6 @@
-import React from "react";
-import { AboutTextContainer, IntroText, IntroTextContainer } from './AboutText.style';
+import { AboutTextContainer, IntroTextContainer } from './AboutText.style';
 
 const AboutText = () => {
-    const intro1 = `I am an application developer with nearly 3 years experience in working on a <b>software development lifecycle (SDLC)</b>`
-    const intro4 = "I have worked on <b>analysis, design, development</b> and <b>testing, deployment and documentation</b> in software development"
-    const intro2 = "I have worked in an agile environment, followed <b>TDD and pair programming</b> when developing a <b>mobile app</b> and creating multiple <b>microservices</b>"
-    const intros = [intro1, intro4, intro2]
     const { innerWidth: width } = window;
 
     return (
